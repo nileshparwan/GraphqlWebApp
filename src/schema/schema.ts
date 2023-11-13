798c4705dba61f8989c6bdf11d9bf6633c3a952a
KoshalParwan <koshal.parwan@fasst.io>
2023-11-13 21:49:44 +0400

    REWORK!!!

--- src/schema/schema.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLSchema } from 'graphql';
-import { RootQuery } from './query';
-
-export default new GraphQLSchema({
-    query: RootQuery
-});
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,9 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 
-// internal
-import schema from '@schema/schema';
-
 export const init = async () => {
   // variables
   const app = express();
-
-  // middleware
-  app.use(
-    "/graphql",
-    graphqlHTTP({
-      schema,
-      graphiql: true,
-    })
-  )
 
   return app;
 }
