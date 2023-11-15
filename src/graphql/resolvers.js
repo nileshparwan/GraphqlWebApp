2ac3b41e214d026f42614cf09ef8790bbb505125
KoshalParwan <koshal.parwan@fasst.io>
2023-11-15 08:21:03 +0400

    creating custom type

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,36 +1,20 @@
 export const resolvers = {
-    // Query: {
-    //     id() {
-    //         return 'abc12';
-    //     },
-    //     name() {
-    //         return 'koshal';
-    //     },
-    //     age() {
-    //         return 23;
-    //     },
-    //     employed() {
-    //         return true;
-    //     },
-    //     gpa() {
-    //         return 2.1; // can also be null
-    //     }
-    // },
     Query: {
-        title() {
-            return 'title';
+        me() {
+            return {
+                id: "23",
+                name: "Koshal",
+                age: 20,
+                email: "test@test.com"
+            };
         },
-        price() {
-            return 50;
-        },
-        releaseYear() {
-            return null;
-        },
-        rating() {
-            return 5.0;
-        },
-        inStock() {
-            return true;
+        post() {
+            return {
+                id: "092",
+                title: "101",
+                body: "",
+                published: false
+            };
         }
-    }
+    },
 };
--- src/graphql/type.js
+++ src/graphql/type.js
@@ -4,19 +4,41 @@
  * 
 */
 export const typeDefs = /* GraphQL */ `
-    # type Query {
-    #     id: ID!
-    #     name: String!
-    #     age: Int!
-    #     employed: Boolean!
-    #     gpa: Float
-    # }
-
     type Query {
+        me: User!
+        post: Post!
+        product(id: String): Product
+        productionCollection(where: productInput, limit: Int): [ProductCollection]
+    }
+
+    input productInput {
+        id: String!
+        name: String
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        age: Int!
+        email: String!
+    }
+
+    type Product {
         title: String!
         price: Float!
         releaseYear: Int
         rating: Float
         inStock: Boolean!
+    }
+
+    type ProductCollection {
+        items: [Product]
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `;
